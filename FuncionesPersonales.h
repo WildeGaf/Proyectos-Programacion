9329b9155bd2c5122667d98ae73d78730670bf4c
WildeGaf <40776504+WildeGaf@users.noreply.github.com>
2018-09-17 17:55:15 -0300

    Add files via upload

---
+++ FuncionesPersonales.h
@@ -0,0 +1,17 @@
+#ifndef FUNCIONESPERSONALES_H_INCLUDED
+#define FUNCIONESPERSONALES_H_INCLUDED
+
+int menu();
+int suma(int,int);
+int resta(int,int);
+int division(int,int);
+int multiplicacion(int,int);
+int factorial(int);
+int getInt(char* mensaje);
+float getFloat(char* mensaje)
+int validar_getEntero(  int* pEntero, char* msg,
+                        char msgErr[],int min, int max,
+                        int reintentos);
+
+
+#endif // FUNCIONESPERSONALES_H_INCLUDED
